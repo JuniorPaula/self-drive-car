9ecdf47e1ef038d6cd0833b7dc0297f39df7cc29
Junior Paula <luke.junnior@icloud.com>
2023-05-24 13:16:45 -0300

    Create Road class

--- main.mjs
+++ main.mjs
@@ -1,9 +1,11 @@
 import Car from './car.mjs'
+import Road from './road.mjs'
 
 const canvas = document.getElementById("myCanvas")
 canvas.width = 200
 
 const ctx = canvas.getContext("2d")
+const road = new Road(canvas.width/2, canvas.width*0.9)
 const car = new Car(100,100,30,50)
 
 animate()
@@ -12,6 +14,7 @@ function animate() {
     car.update()
     
     canvas.height = window.innerHeight
+    road.draw(ctx)
     car.draw(ctx)
     requestAnimationFrame(animate)
 }
