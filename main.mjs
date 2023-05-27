7cfe17106895c6b577c5fdf0c648895cb0fb23c2
Junior Paula <luke.junnior@icloud.com>
2023-05-27 10:36:21 -0300

    Implements mutate to the neural network

--- main.mjs
+++ main.mjs
@@ -1,4 +1,5 @@
 import Car from './car.mjs'
+import NeuralNetwork from './network.mjs'
 import Road from './road.mjs'
 import Visualizer from './visualizer.mjs'
 
@@ -23,18 +24,29 @@ const networkCtx = networkCanvas.getContext("2d")
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9)
 
-const N = 100
+const N = 1
 const cars = generateCars(N)
 let bestCar = cars[0]
 
 if (localStorage.getItem("bestBrain")) {
-    bestCar.brain = JSON.parse(
-        localStorage.getItem("bestBrain")
-    )
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].brain = JSON.parse(
+            localStorage.getItem("bestBrain")
+        )
+        if (i != 0) {
+            NeuralNetwork.mutate(cars[i].brain, 0.1)
+        }
+    }
 }
 
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
 ]
 
 saveBtn.addEventListener("click", save)
@@ -64,7 +76,7 @@ function animate(time) {
         traffic[i].update(road.borders, [])
     }
 
-    for(let i=0; i < cars.length; i++) {
+    for (let i = 0; i < cars.length; i++) {
         cars[i].update(road.borders, traffic)
     }
 
@@ -86,7 +98,7 @@ function animate(time) {
     }
 
     carCtx.globalAlpha = 0.2
-    for(let i=0; i < cars.length; i++) {
+    for (let i = 0; i < cars.length; i++) {
         cars[i].draw(carCtx, "blue")
     }
     carCtx.globalAlpha = 1
@@ -94,7 +106,7 @@ function animate(time) {
 
     carCtx.restore()
 
-    networkCtx.lineDashOffset=-time/50
+    networkCtx.lineDashOffset = -time / 50
     Visualizer.drawNetwork(networkCtx, bestCar.brain)
 
     requestAnimationFrame(animate)
